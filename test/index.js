c072380f26954a8af6d7a68483200bcb2d3cfeaa
gawao <gawao57@gmail.com>
2024-04-08 18:12:39 +0900

    add test js file

---
+++ test/index.js
@@ -0,0 +1,6 @@
+console.log("----- wf test js file ----");
+console.log("=======▼▼▼▼ echo process.env ===========");
+
+console.log(process.env);
+
+console.log("=======▲▲▲▲ echo process.env ===========");
