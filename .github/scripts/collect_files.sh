043aa89c2a9b8a293da61d73ea07197c75946434
gawao <gawao57@gmail.com>
2024-07-08 16:32:00 +0900

    test(docs-generator): fileリストの渡し方を変更

--- .github/scripts/collect_files.sh
+++ .github/scripts/collect_files.sh
@@ -7,23 +7,23 @@ DOC_TYPE=$1
 
 if [ "${DOC_TYPE}" == "workflow" ]; then
   # Find all YAML files in the workflows directory
-  yaml_files=$(find .github/workflows -name '*.yml' -not -path '*internal*' | jq -R . | jq -s .)
 #   yaml_files="$(find .github/workflows -name '*.yml' -not -path '*internal*')"
+  yaml_files=$(find .github/workflows -name '*.yml' -not -path '*internal*' | paste -sd "," -)
 
   # path/to/file1.yml → docs/file1.md
 #   markdown_files="${yaml_files//.github\/workflows\//docs/}"
 #   markdown_files="${markdown_files//.yml/.md}"
-  markdown_files=$(echo "$yaml_files" | jq -r 'map(gsub(".github/workflows/"; "docs/") | gsub(".yml"; ".md"))')
+  markdown_files=$(echo "$yaml_files" | sed 's/.github\/workflows\//docs\//g' | sed 's/.yml/.md/g')
 
 elif [ "${DOC_TYPE}" == "composite" ]; then
   # Find all YAML files in the composite directory
 #   yaml_files="$(find composite -name 'action.yml')"
-  yaml_files=$(find composite -name 'action.yml' | jq -R . | jq -s .)
+  yaml_files=$(find composite -name 'action.yml' | paste -sd "," -)
 
   # composite/path/to/file1/action.yml → docs/path/to/file1.md
 #   markdown_files="${yaml_files//composite\//docs/}"
 #   markdown_files="${markdown_files//\/action.yml/.md}"
-  markdown_files=$(echo "$yaml_files" | jq -r 'map(gsub("composite/"; "docs/") | gsub("/action.yml"; ".md"))')
+  markdown_files=$(echo "$yaml_files" | sed 's/composite\//docs\//g' | sed 's/\/action.yml/.md/g')
 
 else
   echo "::error::Invalid doc-type value '${DOC_TYPE}'. It must be either 'workflow' or 'composite'."
@@ -34,11 +34,7 @@ fi
 # echo "yaml-files=$(echo "$yaml_files" | jq -Rsc 'split("\n") | map(select(. != ""))')" >> "$GITHUB_OUTPUT"
 # echo "markdown-files=$(echo "$markdown_files" | jq -Rsc 'split("\n") | map(select(. != ""))')" >> "$GITHUB_OUTPUT"
 
-# Remove newlines and output as single line
-yaml_files_single_line=$(echo "$yaml_files" | jq -c .)
-markdown_files_single_line=$(echo "$markdown_files" | jq -c .)
-
-echo "yaml-files=$yaml_files_single_line" >> "$GITHUB_OUTPUT"
-echo "markdown-files=$markdown_files_single_line" >> "$GITHUB_OUTPUT"
+echo "yaml-files=$yaml_files" >> "$GITHUB_OUTPUT"
+echo "markdown-files=$markdown_files" >> "$GITHUB_OUTPUT"
 
 echo "::endgroup::"
--- .github/scripts/generate_docs.sh
+++ .github/scripts/generate_docs.sh
@@ -9,12 +9,24 @@ MARKDOWN_FILES=$2
 # Use jq to parse the JSON array properly and remove unwanted characters
 # yaml_files_str=$(echo "${YAML_FILES:-}" | jq -r '.[]')
 # markdown_files_str=$(echo "${MARKDOWN_FILES:-}" | jq -r '.[]')
-yaml_files_str=$(echo "${YAML_FILES}" | jq -r '.[]')
-markdown_files_str=$(echo "${MARKDOWN_FILES}" | jq -r '.[]')
 
-# Convert newline-separated strings to arrays using mapfile
-mapfile -t yaml_files <<< "$yaml_files_str"
-mapfile -t markdown_files <<< "$markdown_files_str"
+# カンマ区切りの文字列を配列に変換
+IFS=',' read -r -a yaml_files <<< "$YAML_FILES"
+IFS=',' read -r -a markdown_files <<< "$MARKDOWN_FILES"
+
+echo "YAML files array:"
+for file in "${yaml_files[@]}"; do
+  echo "$file"
+done
+
+echo "Markdown files array:"
+for file in "${markdown_files[@]}"; do
+  echo "$file"
+done
+
+# # Convert newline-separated strings to arrays using mapfile
+# mapfile -t yaml_files <<< "$yaml_files_str"
+# mapfile -t markdown_files <<< "$markdown_files_str"
 
 for index in "${!yaml_files[@]}"; do
   yaml_file=${yaml_files[index]}
