1ae8cba90438333915b6f309e31424677482c302
gawao <gawao57@gmail.com>
2024-07-08 14:51:56 +0900

    refactor(docs-generator): yaml, markdown file リスト作成をshell script に持っていく

---
+++ .github/scripts/collect_files.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -xeu
+
+echo "::group::Collect YAML and Markdown files"
+
+DOC_TYPE=$1
+
+if [ "${DOC_TYPE}" == "workflow" ]; then
+  # Find all YAML files in the workflows directory
+  yaml_files="$(find .github/workflows -name '*.yml' -not -path '*internal*')"
+
+  # path/to/file1.yml → docs/file1.md
+  markdown_files="${yaml_files//.github\/workflows\//docs/}"
+  markdown_files="${markdown_files//.yml/.md}"
+
+elif [ "${DOC_TYPE}" == "composite" ]; then
+  # Find all YAML files in the composite directory
+  yaml_files="$(find composite -name 'action.yml')"
+
+  # composite/path/to/file1/action.yml → docs/path/to/file1.md
+  markdown_files="${yaml_files//composite\//docs/}"
+  markdown_files="${markdown_files//\/action.yml/.md}"
+
+else
+  echo "::error::Invalid doc-type value '${DOC_TYPE}'. It must be either 'workflow' or 'composite'."
+  exit 1
+fi
+
+# Output the YAML and Markdown files as JSON arrays
+echo "yaml-files=$(echo "$yaml_files" | jq -Rsc 'split("\n") | map(select(. != ""))')" >> "$GITHUB_OUTPUT"
+echo "markdown-files=$(echo "$markdown_files" | jq -Rsc 'split("\n") | map(select(. != ""))')" >> "$GITHUB_OUTPUT"
+
+echo "::endgroup::"
